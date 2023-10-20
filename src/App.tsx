ccbeb6bd70a95653f4f0dd495d2b2c2399c78956
Potato <poltoradnevone@gmail.com>
2023-10-20 09:40:05 +0400

    feat: added model improvement

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { useEffect, useState, useCallback, useMemo } from "react";
+import { useEffect, useState, useCallback, useMemo, Fragment } from "react";
 import {
   CartesianGrid,
   Line,
@@ -18,6 +18,8 @@ const colors = ["#8884d8", "#a5b4fc", "#6366f1", "#2563eb", "#312e81"];
 function App() {
   const [numberOfPopulations, setNumberOfPopulations] = useState(2);
 
+  const [deseaseSpawnrate, setDeseaseSpawnrate] = useState(0);
+
   const [coeffs, setCoeffs] = useState<number[][]>(
     new Array(numberOfPopulations)
       .fill(new Array(numberOfPopulations).fill(0.0001))
@@ -25,6 +27,14 @@ function App() {
         row.map((num: number, colIdx: number) => (rowIdx === colIdx ? 0 : num))
       )
   );
+
+  const [deseaseResistanceCoeffs, setDeseaseResistanceCoeffs] = useState<
+    number[]
+  >(new Array(numberOfPopulations).fill(1));
+
+  const [seasonsDependency, setSeasonsDependency] = useState(false);
+
+  const [seasonsDependencyCoeff, setSeasonsDependecyCoeff] = useState(2);
 
   const [simulationStep, setSimulationStep] = useState(1);
 
@@ -42,7 +52,7 @@ function App() {
         day: 0,
       } as Record<string, number>;
       for (let i = 0; i < numberOfPopulations; ++i) {
-        obj[i] = populations[i].amount;
+        obj[i] = populations[i]?.amount ?? 100;
       }
       return obj;
     })
@@ -73,6 +83,7 @@ function App() {
           )
         )
     );
+    setDeseaseResistanceCoeffs(new Array(numberOfPopulations).fill(1));
   }, [numberOfPopulations]);
 
   useEffect(() => {
@@ -82,12 +93,19 @@ function App() {
           day: 0,
         } as Record<string, number>;
         for (let i = 0; i < numberOfPopulations; ++i) {
-          obj[i] = populations[i].amount;
+          obj[i] = populations[i]?.amount ?? 100;
         }
         return obj;
       })
     );
-  }, [numberOfPopulations, populations]);
+  }, [populations, numberOfPopulations]);
+
+  const handleSeasonsDependencyChange = useCallback(
+    (event: React.ChangeEvent<HTMLInputElement>) => {
+      setSeasonsDependency(event.target.checked);
+    },
+    []
+  );
 
   const handleNumberOfPopulationsChange = useCallback(
     (event: React.ChangeEvent<HTMLInputElement>) => {
@@ -106,6 +124,20 @@ function App() {
   const handleSimulationStepChange = useCallback(
     (event: React.ChangeEvent<HTMLInputElement>) => {
       setSimulationStep(+event.target.value);
+    },
+    []
+  );
+
+  const handleSeasonsDependencyCoeffChange = useCallback(
+    (event: React.ChangeEvent<HTMLInputElement>) => {
+      setSeasonsDependecyCoeff(+event.target.value);
+    },
+    []
+  );
+
+  const handleDeseaseSpawnrateChange = useCallback(
+    (event: React.ChangeEvent<HTMLInputElement>) => {
+      setDeseaseSpawnrate(+event.target.value);
     },
     []
   );
@@ -134,11 +166,33 @@ function App() {
         for (let k = 0; k < simulationStep; ++k) {
           for (let i = 0; i < numberOfPopulations; ++i) {
             const N = newData[i];
-            let dN = N * populations[i].growth;
+            let growth = populations[i].growth;
+            const currentDay = day % 365;
+            /* Winter check */
+            if (seasonsDependency && (currentDay < 58 || currentDay >= 335)) {
+              growth =
+                growth > 0
+                  ? growth * (1 / seasonsDependencyCoeff)
+                  : growth * seasonsDependencyCoeff;
+              /* Summer check */
+            } else if (
+              seasonsDependency &&
+              currentDay >= 149 &&
+              currentDay <= 239
+            ) {
+              growth =
+                growth > 0
+                  ? growth * seasonsDependencyCoeff
+                  : growth * (1 / seasonsDependencyCoeff);
+            }
+            let dN = N * growth;
             for (let j = 0; j < numberOfPopulations; ++j) {
               dN += coeffs[i][j] * N * newData[j];
             }
             newData[i] = N + dN;
+            if (Math.random() < deseaseSpawnrate) {
+              newData[i] *= deseaseResistanceCoeffs[i];
+            }
           }
         }
         return [...p, newData];
@@ -158,166 +212,242 @@ function App() {
     coeffs,
     start,
     simulationStep,
+    seasonsDependency,
+    seasonsDependencyCoeff,
+    deseaseSpawnrate,
+    deseaseResistanceCoeffs,
   ]);
 
   return (
-    <div
-      style={{
-        display: "flex",
-      }}
-    >
-      <div style={{ display: "flex", flexDirection: "column", padding: 20 }}>
-        <label
-          style={{ display: "flex", flexDirection: "column" }}
-          htmlFor="numberOfPopulations"
-        >
-          number of populations
-          <input
-            type="number"
-            id="numberOfPopulations"
-            value={numberOfPopulations}
-            onChange={handleNumberOfPopulationsChange}
-            placeholder="number of populations"
-          />
-        </label>
-        <div
-          style={{
-            width: "100%",
-            height: 1,
-            backgroundColor: "#ccc",
-            margin: "16px 0",
-          }}
-        />
-        {populations.map((population, index) => (
-          <div key={index} style={{ marginTop: index ? 16 : 0 }}>
-            <p>population number {index + 1}</p>
-            <label
-              style={{ display: "flex", flexDirection: "column" }}
-              htmlFor={`amount${index}`}
-            >
-              population amount
-              <input
-                type="number"
-                id={`amount${index}`}
-                value={population.amount}
-                onChange={(event) =>
-                  setPopulations((p) =>
-                    p.map((p, idx) =>
-                      idx === index
-                        ? {
-                            ...p,
-                            amount: +event.target.value,
-                          }
-                        : p
-                    )
-                  )
-                }
-                placeholder="number of populations"
-              />
-            </label>
-            <label
-              style={{
-                display: "flex",
-                flexDirection: "column",
-                marginTop: 8,
-              }}
-              htmlFor={`growth${index}`}
-            >
-              population growth
-              <input
-                type="number"
-                id={`growth${index}`}
-                value={population.growth}
-                onChange={(event) =>
-                  setPopulations((p) =>
-                    p.map((p, idx) =>
-                      idx === index
-                        ? {
-                            ...p,
-                            growth: +event.target.value,
-                          }
-                        : p
-                    )
-                  )
-                }
-                placeholder="number of populations"
-              />
-            </label>
-          </div>
-        ))}
-      </div>
+    <Fragment>
       <div
         style={{
-          marginLeft: 20,
           display: "flex",
-          flexDirection: "column",
-          padding: 20,
         }}
       >
-        {coeffs.map((row, rowIdx) => (
-          <div key={rowIdx} style={{ display: "flex" }}>
-            {row.map((coeff, colIdx) => (
-              <input
-                key={`${rowIdx}${colIdx}`}
-                onChange={(e) =>
-                  setCoeffs((p) =>
-                    p.map((row, rowIndex) =>
-                      row.map((num, colIndex) =>
-                        rowIdx === rowIndex && colIdx === colIndex
-                          ? +e.target.value
-                          : num
+        <div
+          style={{
+            display: "flex",
+            flexDirection: "column",
+            padding: 20,
+          }}
+        >
+          <label
+            style={{ display: "flex", flexDirection: "column" }}
+            htmlFor="numberOfPopulations"
+          >
+            number of populations
+            <input
+              type="number"
+              id="numberOfPopulations"
+              value={numberOfPopulations}
+              onChange={handleNumberOfPopulationsChange}
+              placeholder="number of populations"
+            />
+          </label>
+          <div
+            style={{
+              width: "100%",
+              height: 1,
+              backgroundColor: "#ccc",
+              margin: "16px 0",
+            }}
+          />
+          {populations.map((population, index) => (
+            <div key={index} style={{ marginTop: index ? 16 : 0 }}>
+              <p>population number {index + 1}</p>
+              <label
+                style={{ display: "flex", flexDirection: "column" }}
+                htmlFor={`amount${index}`}
+              >
+                population amount
+                <input
+                  type="number"
+                  id={`amount${index}`}
+                  value={population.amount}
+                  onChange={(event) =>
+                    setPopulations((p) =>
+                      p.map((p, idx) =>
+                        idx === index
+                          ? {
+                              ...p,
+                              amount: +event.target.value,
+                            }
+                          : p
                       )
+                    )
+                  }
+                  placeholder="number of populations"
+                />
+              </label>
+              <label
+                style={{
+                  display: "flex",
+                  flexDirection: "column",
+                  marginTop: 8,
+                }}
+                htmlFor={`growth${index}`}
+              >
+                population growth
+                <input
+                  type="number"
+                  id={`growth${index}`}
+                  value={population.growth}
+                  onChange={(event) =>
+                    setPopulations((p) =>
+                      p.map((p, idx) =>
+                        idx === index
+                          ? {
+                              ...p,
+                              growth: +event.target.value,
+                            }
+                          : p
+                      )
+                    )
+                  }
+                  placeholder="number of populations"
+                />
+              </label>
+            </div>
+          ))}
+        </div>
+        <div
+          style={{
+            marginLeft: 20,
+            display: "flex",
+            flexDirection: "column",
+            padding: 20,
+          }}
+        >
+          <div>population interaction coefficients</div>
+          {coeffs.map((row, rowIdx) => (
+            <div key={rowIdx} style={{ display: "flex" }}>
+              {row.map((coeff, colIdx) => (
+                <input
+                  key={`${rowIdx}${colIdx}`}
+                  onChange={(e) =>
+                    setCoeffs((p) =>
+                      p.map((row, rowIndex) =>
+                        row.map((num, colIndex) =>
+                          rowIdx === rowIndex && colIdx === colIndex
+                            ? +e.target.value
+                            : num
+                        )
+                      )
+                    )
+                  }
+                  type="number"
+                  value={coeff}
+                />
+              ))}
+            </div>
+          ))}
+          <label
+            style={{ marginTop: 20, display: "flex", flexDirection: "column" }}
+            htmlFor="simulationDuration"
+          >
+            simulation duration amount
+            <input
+              id="sumulationDuration"
+              type="number"
+              placeholder="days"
+              value={simulationDuration}
+              onChange={handleSimulationDurationChange}
+            />
+          </label>
+          <label
+            htmlFor="simulationStep"
+            style={{ marginTop: 20, display: "flex", flexDirection: "column" }}
+          >
+            simulation step
+            <input
+              id="sumulationStep"
+              type="number"
+              placeholder="step"
+              value={simulationStep}
+              onChange={handleSimulationStepChange}
+            />
+          </label>
+          <button style={{ marginTop: 16 }} onClick={handleClick}>
+            {start ? "stop" : "start"}
+          </button>
+          <div style={{ marginTop: 16 }}>
+            <p>simulation information: </p>
+            <p>total population amount {totalPopulationAmount.toFixed(2)}</p>
+            <p>current day {data[data.length - 1].day}</p>
+          </div>
+        </div>
+        <div
+          style={{ marginTop: 16, display: "flex", flexDirection: "column" }}
+        >
+          <label
+            htmlFor="seasonsDependency"
+            style={{ display: "inline-flex", alignItems: "center" }}
+          >
+            seasons dependency
+            <input
+              id="seasonsDependency"
+              type="checkbox"
+              checked={seasonsDependency}
+              onChange={handleSeasonsDependencyChange}
+              style={{ marginLeft: 10 }}
+            />
+          </label>
+          <label
+            htmlFor="seasonsDependecy"
+            style={{ marginTop: 16, display: "flex", flexDirection: "column" }}
+          >
+            seasons dependency coeff
+            <input
+              id="seasonsDependency"
+              type="number"
+              value={seasonsDependencyCoeff}
+              onChange={handleSeasonsDependencyCoeffChange}
+            />
+          </label>
+        </div>
+        <div style={{ marginTop: 16, marginLeft: 20 }}>
+          <label
+            htmlFor="deseaseSpawnrate"
+            style={{ display: "flex", flexDirection: "column" }}
+          >
+            illness spawnrate
+            <input
+              type="number"
+              min={0}
+              max={1}
+              step={0.01}
+              id="deseaseSpawnrate"
+              value={deseaseSpawnrate}
+              onChange={handleDeseaseSpawnrateChange}
+            />
+          </label>
+          <label style={{ display: "flex", flexDirection: "column" }}>
+            illness resistance coeffs
+            {deseaseResistanceCoeffs.map((value, index) => (
+              <input
+                key={index}
+                type="number"
+                placeholder={`Population ${index + 1}`}
+                value={value}
+                onChange={(event) =>
+                  setDeseaseResistanceCoeffs((p) =>
+                    p.map((value, valueIndex) =>
+                      index === valueIndex ? +event.target.value : value
                     )
                   )
                 }
-                type="number"
-                value={coeff}
               />
             ))}
-          </div>
-        ))}
-        <label
-          style={{ marginTop: 20, display: "flex", flexDirection: "column" }}
-          htmlFor="simulationDuration"
-        >
-          simulation duration amount
-          <input
-            id="sumulationDuration"
-            type="number"
-            placeholder="days"
-            value={simulationDuration}
-            onChange={handleSimulationDurationChange}
-          />
-        </label>
-        <label
-          htmlFor="simulationStep"
-          style={{ marginTop: 20, display: "flex", flexDirection: "column" }}
-        >
-          simulation step
-          <input
-            id="sumulationStep"
-            type="number"
-            placeholder="step"
-            value={simulationStep}
-            onChange={handleSimulationStepChange}
-          />
-        </label>
-        <button style={{ marginTop: 16 }} onClick={handleClick}>
-          {start ? "stop" : "start"}
-        </button>
-        <div style={{ marginTop: 16 }}>
-          <p>simulation information: </p>
-          <p>total population amount {totalPopulationAmount.toFixed(2)}</p>
-          <p>current day {data[data.length - 1].day}</p>
+          </label>
         </div>
       </div>
-
       <LineChart
-        width={600}
-        height={250}
+        width={1000}
+        height={300}
         data={data}
         margin={{
-          top: 5,
+          top: 50,
           right: 30,
           left: 20,
           bottom: 5,
@@ -329,6 +459,7 @@ function App() {
         <Tooltip />
         {populations.map((_, index) => (
           <Line
+            key={index}
             type="monotone"
             dataKey={index}
             stroke={colors[index % colors.length]}
@@ -336,7 +467,7 @@ function App() {
           />
         ))}
       </LineChart>
-    </div>
+    </Fragment>
   );
 }
 
